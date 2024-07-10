b153484df19d1a152e79900b47131704dc333f94
kenvofc <148454617+kenvofc@users.noreply.github.com>
2024-07-10 16:28:38 +0100

    Update itxxwasi.js

--- itxxwasi.js
+++ itxxwasi.js
@@ -3,16 +3,11 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qrr'),
-    code = require('./pair');
+let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/wasiqr', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/wasipage.html')
 })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
